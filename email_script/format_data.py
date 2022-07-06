f21a1177276235e18bc5fab18c1232140ebdbd03
AlexAzalor <yablunovsky.a@gmail.com>
2022-07-06 14:34:08 +0300

    add images

--- email_script/format_data.py
+++ email_script/format_data.py
@@ -1,3 +1,5 @@
+from email import encoders
+from email.mime.base import MIMEBase
 import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -20,7 +22,7 @@ class FormatData:
         self.msg = MIMEMultipart()
         self.msg["From"] = SMTP_EMAIL
         self.msg["To"] = data_from_xml.email_address
-        self.msg["Subject"] = "Some title"
+        self.msg["Subject"] = "פינוויז מגלה לכם מה תהיה עמלת הפרעון מהוקדם שלכם"
         self.data = {
             "email_address": data_from_xml.email_address,
             "prepayment_calc": "{:,}".format(int(data_from_xml.prepayment_calc)),
@@ -48,6 +50,36 @@ class FormatData:
             letter_text = template.render(a=self.data)
 
             self.msg.attach(MIMEText(letter_text, "html", _charset="utf-8"))
+            with open("email_script/myimagecid.png", "rb") as f:
+                # set attachment mime and file name, the image type is png
+                mime = MIMEBase("image", "png", filename="myimagecid.png")
+                # add required header data:
+                mime.add_header(
+                    "Content-Disposition", "attachment", filename="myimagecid.png"
+                )
+                mime.add_header("X-Attachment-Id", "0")
+                mime.add_header("Content-ID", "<0>")
+                # read attachment file content into the MIMEBase object
+                mime.set_payload(f.read())
+                # encode with base64
+                encoders.encode_base64(mime)
+                # add MIMEBase object to MIMEMultipart object
+                self.msg.attach(mime)
+            with open("email_script/finwiz-logo.png", "rb") as f:
+                # set attachment mime and file name, the image type is png
+                mime = MIMEBase("image", "png", filename="finwiz-logo.png")
+                # add required header data:
+                mime.add_header(
+                    "Content-Disposition", "attachment", filename="finwiz-logo.png"
+                )
+                mime.add_header("X-Attachment-Id", "1")
+                mime.add_header("Content-ID", "<1>")
+                # read attachment file content into the MIMEBase object
+                mime.set_payload(f.read())
+                # encode with base64
+                encoders.encode_base64(mime)
+                # add MIMEBase object to MIMEMultipart object
+                self.msg.attach(mime)
 
         except Exception as e:
             print(e)
